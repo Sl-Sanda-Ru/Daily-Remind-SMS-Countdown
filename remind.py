8ea2085c04166fa9ce6477517ede56832222b8a2
Sandaru Ashen <sandaruashen02@gmail.com>
2021-06-02 22:23:15 +0800

    com

--- remind.py
+++ remind.py
@@ -8,13 +8,13 @@ year = int(environ['DAY'].split('-')[0])
 month = int(environ['DAY'].split('-')[1].lstrip('0'))
 day = int(environ['DAY'].split('-')[2].lstrip('0'))
 count_day = date(year,month,day)
-greets = ['Good Morning!','Hello There!','සුභ උදෑසනක්','නැගිටපන් යකෝ']
+greets = environ['GRETTS'].split(',')
 def sms():
 	tday = datetime.utcnow() + timedelta(hours=5,minutes=30)
 	tday = tday.date()
 	rem = count_day - tday
 	rem = rem.days
-	data = {'phone':environ['NUM'],'message':f'{choice(greets)} ඒලෙවල් වලට තව දින {rem}යි.','key':'textbelt'}
+	data = {'phone':environ['NUM'],'message':f'{choice(greets)} {message} {rem}යි.','key':'textbelt'}
 	req = post('https://textbelt.com/text',data=data)
 	print(req.json())
 schedule.every().day.at(environ['TIME']).do(sms)
